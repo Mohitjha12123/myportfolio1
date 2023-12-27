3d34d9a70d5d6c80ec83a55d91421a0e417a06d6
Mohitjha12123 <mohitjha7777777@gmail.com>
2023-12-27 13:36:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const mohit = document.getElementById("hamberger")
+const avni = document.getElementById("navlist")
+
+mohit.addEventListener("click", () => {
+
+    avni.classList.toggle("navlist-active")
+})
